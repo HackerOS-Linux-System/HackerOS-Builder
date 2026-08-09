9957f5aee8aab7e6021818ed3a867ee939e1fe9e
michal92299 <chudybyczek2137@gmail.com>
2026-08-09 19:06:01 +0200

    Update pull.go

--- internal/ociimage/pull.go
+++ internal/ociimage/pull.go
@@ -2,11 +2,14 @@ package ociimage
 
 import (
 	"archive/tar"
+	"errors"
 	"fmt"
 	"io"
+	"net/http"
 	"os"
 	"path/filepath"
 	"strings"
+	"time"
 
 	v1 "github.com/google/go-containerregistry/pkg/v1"
 
@@ -17,6 +20,19 @@ import (
 	"github.com/HackerOS-Linux-System/hackeros-builder/internal/httpclient"
 	"github.com/HackerOS-Linux-System/hackeros-builder/internal/util"
 )
+
+// maxPullAttempts to liczba prob sciagniecia+rozpakowania obrazu zanim
+// PullAndUnpack odda blad uzytkownikowi. Rejestrujaco: sam registry (ghcr.io)
+// oraz posrednie proxy potrafia ucinac polaczenie w polowie strumienia danych
+// pod obciazeniem/przy dluzszych transferach -- to sie objawia jako
+// "unexpected EOF" w SRODKU kopiowania konkretnego pliku warstwy (a nie na
+// granicy naglowka tar), czyli klasyczny objaw zerwanego polaczenia
+// sieciowego, NIE uszkodzonych danych zbudowanych lokalnie (to osobny
+// przypadek, zabezpieczony juz w push.go przez validateLayerTarball).
+const maxPullAttempts = 4
+
+// pullRetryBackoff to czas oczekiwania miedzy kolejnymi probami.
+var pullRetryBackoff = 3 * time.Second
 
 // PullParams to dane potrzebne do sciagniecia obrazu OCI z registry.
 type PullParams struct {
@@ -37,6 +53,13 @@ type PullParams struct {
 // Uzywane przez "hackeros-builder build iso", ktore buduje ISO z obrazu
 // OCI juz znajdujacego sie w registry -- gwarantuje to, ze ISO jest
 // dokladnym odzwierciedleniem tego co zostalo opublikowane przez "build cloud".
+//
+// Cala operacja (fetch obrazu + rozpakowanie WSZYSTKICH warstw) jest ponawiana
+// az do maxPullAttempts razy, jesli blad wyglada na przejsciowy problem
+// sieciowy (zerwane polaczenie w trakcie strumieniowania). Kazda proba
+// zaczyna od ZERA -- swiezy uchwyt v1.Image/v1.Layer z registry, wyczyszczony
+// DestDir -- bo ponawianie odczytu na tym samym, juz zerwanym strumieniu
+// HTTP nie ma sensu (polaczenie jest martwe, trzeba nowego).
 func PullAndUnpack(p PullParams) error {
 	refStr := fmt.Sprintf("%s:%s", p.Repository, p.Tag)
 	ref, err := name.ParseReference(refStr)
@@ -44,20 +67,45 @@ func PullAndUnpack(p PullParams) error {
 		return fmt.Errorf("nieprawidlowa referencja obrazu %q: %w", refStr, err)
 	}
 
-	util.Infof("Sciagam obraz OCI: %s", refStr)
 	auth := &authn.Basic{Username: "hackeros-builder", Password: p.Token}
 	httpClient := httpclient.NewForRegistry(p.Insecure)
 
+	var lastErr error
+	for attempt := 1; attempt <= maxPullAttempts; attempt++ {
+		if attempt > 1 {
+			util.Warnf("Sciaganie obrazu nie powiodlo sie (proba %d/%d): %v -- ponawiam za %s...",
+				attempt-1, maxPullAttempts, lastErr, pullRetryBackoff)
+			time.Sleep(pullRetryBackoff)
+		}
+		util.Infof("Sciagam obraz OCI: %s (proba %d/%d)", refStr, attempt, maxPullAttempts)
+
+		err := pullAndUnpackOnce(ref, auth, httpClient, p.DestDir)
+		if err == nil {
+			util.Infof("Obraz rozpakowany do %s", p.DestDir)
+			return nil
+		}
+		lastErr = err
+		if !isRetryableTransferErr(err) {
+			return err
+		}
+	}
+	return fmt.Errorf("sciaganie obrazu %s nie powiodlo sie po %d probach (ostatni blad): %w",
+		refStr, maxPullAttempts, lastErr)
+}
+
+// pullAndUnpackOnce to pojedyncza proba PullAndUnpack -- wydzielona zeby
+// PullAndUnpack mogl ja ponawiac ze swiezym stanem przy kazdej probie.
+func pullAndUnpackOnce(ref name.Reference, auth authn.Authenticator, httpClient *http.Client, destDir string) error {
 	img, err := remote.Image(ref, remote.WithAuth(auth), remote.WithTransport(httpClient.Transport))
 	if err != nil {
-		return fmt.Errorf("pobieranie %s nie powiodlo sie: %w", refStr, err)
+		return fmt.Errorf("pobieranie %s nie powiodlo sie: %w", ref.String(), err)
 	}
 
-	if err := os.RemoveAll(p.DestDir); err != nil {
-		return fmt.Errorf("czyszczenie %s: %w", p.DestDir, err)
+	if err := os.RemoveAll(destDir); err != nil {
+		return fmt.Errorf("czyszczenie %s: %w", destDir, err)
 	}
-	if err := os.MkdirAll(p.DestDir, 0o755); err != nil {
-		return fmt.Errorf("tworzenie %s: %w", p.DestDir, err)
+	if err := os.MkdirAll(destDir, 0o755); err != nil {
+		return fmt.Errorf("tworzenie %s: %w", destDir, err)
 	}
 
 	layers, err := img.Layers()
@@ -67,13 +115,39 @@ func PullAndUnpack(p PullParams) error {
 
 	util.Infof("Rozpakowywanie %d warstw(y)...", len(layers))
 	for i, layer := range layers {
-		if err := extractLayer(layer, p.DestDir); err != nil {
+		if err := extractLayer(layer, destDir); err != nil {
 			return fmt.Errorf("rozpakowywanie warstwy %d/%d: %w", i+1, len(layers), err)
 		}
 	}
-
-	util.Infof("Obraz rozpakowany do %s", p.DestDir)
 	return nil
+}
+
+// isRetryableTransferErr rozpoznaje bledy typowe dla zerwanego/przejsciowo
+// niestabilnego polaczenia siecowego z registry -- odrozniamy je od bledow
+// trwalych (401/403 autoryzacji, 404/MANIFEST_UNKNOWN zlej nazwy/tagu,
+// bledny URL) ktore ponawianie i tak by nie naprawilo, wiec od razu oddajemy
+// je uzytkownikowi zamiast marnowac czas na maxPullAttempts prob z gory
+// skazanych na ten sam wynik.
+func isRetryableTransferErr(err error) bool {
+	if errors.Is(err, io.ErrUnexpectedEOF) || errors.Is(err, io.EOF) {
+		return true
+	}
+	msg := strings.ToLower(err.Error())
+	for _, needle := range []string{
+		"unexpected eof",
+		"connection reset",
+		"broken pipe",
+		"timeout",
+		"tls handshake",
+		"i/o timeout",
+		"eof",
+		"temporary failure",
+	} {
+		if strings.Contains(msg, needle) {
+			return true
+		}
+	}
+	return false
 }
 
 // extractLayer rozpakowuje pojedyncza warstwe OCI (tar, zdekompresowany
@@ -139,7 +213,7 @@ func extractLayer(layer v1.Layer, destDir string) error {
 			}
 			if _, err := io.Copy(f, tr); err != nil {
 				f.Close()
-				return err
+				return fmt.Errorf("kopiowanie zawartosci %q ze strumienia warstwy: %w", entryName, err)
 			}
 			f.Close()
 		case tar.TypeSymlink:
