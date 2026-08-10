21d63690c590d8acfa5e28356b88774581d4ae7c
michal92299 <chudybyczek2137@gmail.com>
2026-08-10 08:06:55 +0200

    Update hammer_deps.go

--- internal/rootfs/hammer_deps.go
+++ internal/rootfs/hammer_deps.go
@@ -6,78 +6,80 @@ import (
 	"github.com/HackerOS-Linux-System/hackeros-builder/internal/util"
 )
 
-// debOstreeDeps to lista pakietow apt instalowanych wewnatrz rootfs po
-// wstrzyknieciu binarki deb-ostree -- zawiera biblioteki dynamiczne
-// (shared libraries) wymagane przez deb-ostree do uruchomienia.
+// hammerDeps to lista pakietow apt instalowanych wewnatrz rootfs po
+// wstrzyknieciu binarki hammer -- zawiera biblioteki dynamiczne (shared
+// libraries) wymagane przez hammer do uruchomienia.
 //
-// Skad pochodzi ta lista:
+// Skad pochodzi ta lista: NIE jest to zgadywanie -- wydanie hammer v0.6.0
+// (oci-mode.tar.gz) zostalo pobrane i sprawdzone bezposrednio narzedziem
+// `readelf -d hammer | grep NEEDED` podczas rozwoju hackeros-builder.
+// Faktyczna lista NEEDED z binarki:
 //
-//   - libsolv1        -- resolver zaleznosci (komunikat bledu uzytkownika:
-//     "libsolv.so.1: cannot open shared object file")
-//   - libostree-1-1   -- biblioteka ostree (glowna zależnosc deb-ostree;
-//     dostarcza libostree-1.so.1)
-//   - libarchive13t64 -- archiwizacja (wymagana przez libostree)
-//   - libcurl3t64-gnutls -- HTTP dla pobierania aktualizacji ostree
-//   - libglib2.0-0t64 -- GLib (wymagana przez libostree: GObject, GVariant)
-//   - libgpgme11t64   -- GPG do weryfikacji podpisow commitow ostree
-//   - libselinux1     -- SELinux labeling (wymagana przez libostree nawet
-//     gdy SELinux nie jest aktywny -- libostree linkuje
-//     dynamicznie do libselinux, nawet na systemach bez
-//     SELinux linker musi znalezc biblioteke)
-//   - libsystemd0     -- integracja z journald / sd-bus (logowanie deb-ostree)
+//	libostree-1.so.1     -> libostree-1-1      (rdzen atomowosci/deploymentow hammer)
+//	libgio-2.0.so.0      -> libglib2.0-0t64    (GLib/GObject/GIO, wymagane przez libostree)
+//	libgobject-2.0.so.0  -> libglib2.0-0t64
+//	libglib-2.0.so.0     -> libglib2.0-0t64
+//	liblzma.so.5         -> liblzma5           (dekompresja warstw ostree/OCI)
+//	libbz2.so.1.0        -> libbz2-1.0
+//	libgcc_s.so.1        -> libgcc-s1
+//	libm.so.6            -> libc6 (baza -- zawsze obecna po debootstrap)
+//	libc.so.6            -> libc6 (baza -- zawsze obecna po debootstrap)
+//	ld-linux-x86-64.so.2 -> libc6 (baza -- zawsze obecna po debootstrap)
 //
-// WAZNE: ta lista bedzie rozszerzana w miare odkrywania kolejnych brakujacych
-// bibliotek. Zeby ulatwic diagnostyke, builder po zainstalowaniu binarki
-// uruchamia "ldd /usr/bin/deb-ostree" wewnatrz sandbox i wypisuje wynik --
-// brakujace biblioteki (linia "not found") beda widoczne w logach builda.
-var debOstreeDeps = []string{
-	// Bezposrednia zaleznosc zglaszana przez uzytkownika:
-	"libsolv1",
-	// Pozostale typowe zaleznosci deb-ostree (narzedzia ostree-style dla Debiana):
+// Hammer jest napisany w Rust i statycznie linkuje wiekszosc swoich
+// zaleznosci (HTTP/TLS/GPG-Ed25519 sa wbudowane w binarke -- w
+// przeciwienstwie do deb-ostree, hammer NIE wymaga libcurl, libgpgme ani
+// libselinux1 jako bibliotek dynamicznych), wiec ta lista jest znaczaco
+// krotsza niz analogiczna lista dla deb-ostree.
+//
+// WAZNE: jesli przyszle wydania hammer dodadza nowe zaleznosci dynamiczne,
+// ta lista bedzie wymagac aktualizacji. Zeby ulatwic diagnostyke, builder
+// po zainstalowaniu binarki uruchamia "ldd /usr/bin/hammer" wewnatrz
+// sandbox i wypisuje wynik -- brakujace biblioteki (linia "not found")
+// beda widoczne w logach builda.
+var hammerDeps = []string{
 	"libostree-1-1",
-	"libarchive13t64",
-	"libcurl3t64-gnutls",
 	"libglib2.0-0t64",
-	"libgpgme11t64",
-	"libselinux1",
-	"libsystemd0",
 	"liblzma5",
-	"zlib1g",
+	"libbz2-1.0",
+	"libgcc-s1",
 }
 
-// installDebOstreeDeps instaluje biblioteki dynamiczne wymagane przez
-// wstryknieta binarka deb-ostree wewnatrz rootfs przez sandbox (unshare+chroot).
+// installHammerDeps instaluje biblioteki dynamiczne wymagane przez
+// wstrzyknieta binarke hammer wewnatrz rootfs przez sandbox (unshare+chroot).
 //
-// Musi byc wywolane PO injectDebOstree (zeby binarka byla juz w rootfs)
-// i PO tym jak rootfs ma skonfigurowane zrodla apt (debootstrap juz to
-// zapewnia -- /etc/apt/sources.list jest gotowy po kroku 1).
-func (b *Builder) installDebOstreeDeps() error {
-	util.Infof("  deb-ostree: instalacja %d bibliotek dynamicznych...", len(debOstreeDeps))
+// Musi byc wywolane PO injectHammer (zeby binarka byla juz w rootfs) i PO
+// tym jak rootfs ma skonfigurowane zrodla apt (debootstrap juz to
+// zapewnia -- /etc/apt/sources.list jest gotowy po kroku 1). Musi rowniez
+// byc wywolane PRZED removeAptTooling -- to jest ostatnie miejsce w calym
+// przeplywie budowy w ktorym apt-get jest jeszcze potrzebny.
+func (b *Builder) installHammerDeps() error {
+	util.Infof("  hammer: instalacja %d bibliotek dynamicznych...", len(hammerDeps))
 
 	if err := b.sandboxExec("apt-get", "update"); err != nil {
-		return fmt.Errorf("apt-get update przed instalacja deb-ostree deps: %w", err)
+		return fmt.Errorf("apt-get update przed instalacja zaleznosci hammer: %w", err)
 	}
 
 	args := append([]string{
 		"install", "-y", "--no-install-recommends",
 		"-o", "Dpkg::Options::=--force-confdef",
 		"-o", "Dpkg::Options::=--force-confold",
-	}, debOstreeDeps...)
+	}, hammerDeps...)
 
 	if err := b.sandboxExec("apt-get", args...); err != nil {
-		return fmt.Errorf("instalacja bibliotek deb-ostree (%v): %w", debOstreeDeps, err)
+		return fmt.Errorf("instalacja bibliotek hammer (%v): %w", hammerDeps, err)
 	}
 
-	// Weryfikacja: uruchom "ldd /usr/bin/deb-ostree" wewnatrz rootfs i
-	// wypisz wynik -- brakujace biblioteki (linia "=> not found") beda
-	// widoczne w logach builda co ulatwia diagnostyke w przyszlosci.
-	util.Infof("  deb-ostree: weryfikacja bibliotek dynamicznych (ldd)...")
-	if err := b.sandboxExec("ldd", "/usr/bin/deb-ostree"); err != nil {
+	// Weryfikacja: uruchom "ldd /usr/bin/hammer" wewnatrz rootfs i wypisz
+	// wynik -- brakujace biblioteki (linia "=> not found") beda widoczne
+	// w logach builda co ulatwia diagnostyke w przyszlosci.
+	util.Infof("  hammer: weryfikacja bibliotek dynamicznych (ldd)...")
+	if err := b.sandboxExec("ldd", "/usr/bin/hammer"); err != nil {
 		// ldd moze zwrocic status != 0 jesli jakas biblioteka nie zostala
 		// znaleziona -- to jest OSTRZEZENIE, nie blad krytyczny (builder
 		// kontynuuje, uzytkownik widzi ktore .so brakuje w logach wyzej).
-		util.Warnf("ldd /usr/bin/deb-ostree zwrocilo blad -- sprawdz logi powyzej pod katem 'not found'. " +
-			"Brakujace biblioteki mozna dodac do debOstreeDeps w internal/rootfs/debostree_deps.go")
+		util.Warnf("ldd /usr/bin/hammer zwrocilo blad -- sprawdz logi powyzej pod katem 'not found'. " +
+			"Brakujace biblioteki mozna dodac do hammerDeps w internal/rootfs/hammer_deps.go")
 	}
 
 	return nil
